b6ad4d3aedb2a9842e47a068c738c93485ad7b8e
sparc-charleston-hackathon <sparc.charleston.hackathon@gmail.com>
2015-08-28 22:20:46 -0400

    Added Google Map Key

--- app/src/main/java/org/noip/b2/MapsActivity.java
+++ app/src/main/java/org/noip/b2/MapsActivity.java
@@ -60,6 +60,9 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(32.901231, -79.916614)).title("Marker")); // Sparc Front Driveway 32.901231, -79.916614
+
     }
 }
